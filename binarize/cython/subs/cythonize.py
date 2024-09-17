4949fcbfc53bc4b2ce9918af6b38346bb6a80710
Will Nguyen - ML Engineer <will.nguyen@lizai.co>
2024-09-17 17:01:14 +0700

    update cython for multi packages

--- binarize/cython/subs/cythonize.py
+++ binarize/cython/subs/cythonize.py
@@ -4,50 +4,42 @@ import os
 from Cython.Build import cythonize
 from setuptools import find_packages, setup
 
-MAIN_PACKAGE = "loader"
+MAIN_PACKAGES = ["load", "extract"]
 ANCHOR = os.path.dirname(os.path.abspath(__file__))
 
 
 def remove_py_files():
-    for root, dirs, files in os.walk(os.path.join(ANCHOR, MAIN_PACKAGE)):
-        for file in files:
-            # Keep __init__.py if you want
-            if file.endswith('.py') and file != '__init__.py':
-                os.remove(os.path.join(root, file))
+    for package in MAIN_PACKAGES:
+        for root, dirs, files in os.walk(os.path.join(ANCHOR, package)):
+            for file in files:
+                # Keep __init__.py if you want
+                if file.endswith(".py") and file != "__init__.py":
+                    os.remove(os.path.join(root, file))
 
 
-def create_init_files(base_dir):
+def create_init_files(packages):
     """Recursively creates __init__.py files in all directories within base_dir."""
-    for root, dirs, files in os.walk(base_dir):
-        if root.endswith("__pycache__"):
-            continue
-        # Create an __init__.py file in the current directory if it does not exist
-        init_file_path = os.path.join(root, '__init__.py')
-        if not os.path.exists(init_file_path):
-            with open(init_file_path, 'w') as f:
-                f.write("# This file was automatically generated.\n")
-            print(f"Created {init_file_path}")
-        else:
-            print(f"'{init_file_path}' already exists.")
+    for package in packages:
+        base_dir = os.path.join(ANCHOR, package)
+        for root, dirs, files in os.walk(base_dir):
+            if root.endswith("__pycache__"):
+                continue
+            # Create an __init__.py file in the current directory if it does not exist
+            init_file_path = os.path.join(root, "__init__.py")
+            if not os.path.exists(init_file_path):
+                with open(init_file_path, "w") as f:
+                    f.write("# This file was automatically generated.\n")
+                print(f"Created {init_file_path}")
+            else:
+                print(f"'{init_file_path}' already exists.")
 
 
-create_init_files(MAIN_PACKAGE)
+create_init_files(MAIN_PACKAGES)
 setup(
-    name='xt-utils',
-    version='0.0.0',
-    description='LIZAI XT utils',
-    author='penquin17',
-    author_email='will.nguyen@lizai.co',
-    # packages=find_packages(include=['xt_utils', 'xt_utils.*']),
-    # install_requires=[]
-    # extras_require={
-    #     'cuda': []
-    # }
     ext_modules=cythonize(
-        [
-            os.path.join(ANCHOR, f"{MAIN_PACKAGE}/**/*.py")
-        ],
+        [os.path.join(ANCHOR, f"{package}/**/*.py")
+         for package in MAIN_PACKAGES],
         annotate=True,
-        compiler_directives={'always_allow_keywords': True,
-                             'language_level': "3", }),
+        compiler_directives={"always_allow_keywords": True,
+                             "language_level": "3", }),
 )
